5c8a20d604311d66b88dec523c612939cd3392ac
ulyumdzhi <ulyumdzhiev@gmail.com>
2021-09-30 15:20:57 +0300

    update 2

--- api/core/parcer.py
+++ api/core/parcer.py
@@ -1,46 +1,8 @@
 import os
 from dotenv import load_dotenv
-# from joblib import Parallel, delayed
+from api.loader import API_TOKEN
 
-load_dotenv()
-API_TOKEN = os.getenv('TOKEN_GENIUS')
 
-names = ['Gorillaz',
-'Depeche mode',
-'Muse', 'Metallica',
-'Flo rida', 'Grandson',
-'Oomph!', 'Imagine dragons',
-'Michael jackson',
-'Placebo',
-'Blues saraceno',
-'System of a down',
-'Arctic monkeys',
-'Lmfao', 'Starset',
-'Oh wonder', 'Coldplay',
-'The prodigy', 'Disturbed',
-'The neighbourhood', 'Tardigrade inferno',
-'Scorpions', 'Shinoda', 'Fall out boy',
-'Dope', 'Keane', 'Linkin park',
-'Papa roach', 'The cranberries',
-'Neffex', 'Thirty seconds to mars',
-'Drake', 'Billie eilish', 'Queen', 'Ac/dc',
-"Guns'n'roses", 'Eminem', 'Thousand foot krutch',
-'Meg myers', 'Led zeppelin', 'Skindred', 'Rammstein',
-'The pretty reckless', 'Nickelback', 'Bring me the horizon',
-'Halsey', 'Black eyed peas', 'Maroon 5', 'Hollywood undead', 'Nazareth']
-# def name_checker(names):
-#     import lyricsgenius as lg
-#     res = []
-#     for i in names:
-#         try:
-#             genius = lg.Genius(API_TOKEN, skip_non_songs=True, remove_section_headers=True)
-#             name = (genius.search_artist(i, max_songs=1, allow_name_change=True)).name
-#             res.append(name)
-#         except:
-#             print('error')
-#     Parallel(n_jobs=20, verbose=1)(delayed(name_checker)(i) for i in names)
-#
-#     return res
 def get_lyrics(name, k):
     c = 0
     import lyricsgenius as lg
